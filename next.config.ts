bb1cf9bf873941aecab11ab2d73fef17fafa0550
tekr9d3r <tekr9d3r@gmail.com>
2026-03-10 11:41:31 +0100

    Fix Pino Pretty error

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname),
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      'pino-pretty': false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
